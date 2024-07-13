bcc99e433c0c7e2bf880c4837b5ffa8af075e75d
eunwoojeon <wjsdmsdn1787@gmail.com>
2024-07-14 04:13:06 +0900

    feat: CustomInput component 문자열 입력시 byte 유효성 검사 추가
    - frontend
      - CustomInput component 문자열 입력시 byte 유효성 검사 추가

--- todo_app/src/components/CustomInput.tsx
+++ todo_app/src/components/CustomInput.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { RecoilState, useRecoilState } from 'recoil';
 import { CustomInputProps } from '../types/components';
 import styles from './CustomInput.module.css';
 import useInput from '../hooks/useInput';
+import { Buffer } from 'buffer';
 
 const CustomInput: React.FC<CustomInputProps> = ({ text, recoilState }) => {
-  const value = useInput(recoilState);
-
+  const getByteLengthOfString = (str: string) => {
+    const byteLength = Buffer.byteLength(str, 'utf-8');
+    if (10 >= byteLength) return true;
+    return false;
+  }
+  const value = useInput(recoilState, getByteLengthOfString);
+  
   return (
     <div className={styles.customInput}>
       <span>{text}</span>
