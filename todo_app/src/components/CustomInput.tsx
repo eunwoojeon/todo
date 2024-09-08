8fc582bbd7bfe856fd5a35fbed4f3b8505c34408
eunwoojeon <wjsdmsdn1787@gmail.com>
2024-09-09 00:02:09 +0900

    fix: 빈 input 태그의 spaceholder의 byte를 계산하여 표시하는 문제 수정
    - frontend
      - 빈 input 태그의 spaceholder의 byte를 계산하여 표시하는 문제 수정

--- todo_app/src/components/CustomInput.tsx
+++ todo_app/src/components/CustomInput.tsx
@@ -7,12 +7,7 @@ import { TodoInput } from './CustomInput.style';
 const CustomInput: React.FC<CustomInputProps> = ({ text, recoilState }) => {
   const inputRef = useRef<HTMLInputElement>(null);
   const maxByte = 60;
-  const [byte, setByte] = useState(() => {
-    if (text !== '제목' && text !== '할일') {
-      return Buffer.byteLength(text, 'utf-8');
-    }
-    return 0;
-  });
+  const [byte, setByte] = useState(0);
 
   const getByteLengthOfString = (str: string) => {
     const byteLength = Buffer.byteLength(str, 'utf-8');
